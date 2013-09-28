532fd8fce7e4ffa04b9191a9e452bdc585e7f167
unknown <dani.firdaus5235@gmail.com>
2013-09-28 16:53:10 +0700

    commit01

---
+++ src/BiodataKu.java
@@ -0,0 +1,13 @@
+//Perintah penggunaan package versi 01
+package Latihan01.sesi1.bin;
+
+public class BiodataKu 
+{
+  public static void main(String[] data)
+  {
+	System.out.println("Nama                : Dani Firdaus");
+	System.out.println("Alamat              : Cijantung");
+	System.out.println("Golongan Darah      : A");
+	System.out.println("Jenis Kelamain      : Laki laki");
+  }  
+}
