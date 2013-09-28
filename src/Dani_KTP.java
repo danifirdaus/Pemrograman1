5a7c9bc75c4be8bbc72507f617c4027436305a5b
unknown <dani.firdaus5235@gmail.com>
2013-09-28 17:26:57 +0700

    commit01

---
+++ src/Dani_KTP.java
@@ -0,0 +1,20 @@
+//Latihan membuat KTP
+package Latihan01.sesi1.bin;
+
+public class Dani_KTP 
+{
+  public static void main(String[] ktp)
+  {
+	System.out.println("PROPINSI DKI JAKARTA");
+	System.out.println("NIK                 : 3204260109830004");
+	System.out.println("Nama                : Dani Firdaus");
+	System.out.println("Tempat tanggal lahir: 01 September 1983");
+	System.out.println("Alamat              : Cijantung");
+	System.out.println("Golongan Darah      : A");
+	System.out.println("Agama               : Islam");
+	System.out.println("Jenis Kelamain      : Laki laki");
+	System.out.println("Pekerjaan           : Presiden Garut");
+	System.out.println("Kewarganegaraan     : WNI");
+	System.out.println("Berlaku Hingga      : 01 September 2016");
+  }  
+}
