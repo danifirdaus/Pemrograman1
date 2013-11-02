b7c4bddb6dee7f3c3b72c881c0a9d03c26272406
unknown <dani.firdaus5235@gmail.com>
2013-11-02 19:52:56 +0700

    commit02

--- src/DemoLooping2.java
+++ src/DemoLooping2.java
@@ -1,5 +1,11 @@
 //perintah control flow/structur kontrol
 //perintah ketiga - contoh 03
+//E:\Dropbox\kuliah\smester4\Pemrograman 1\sesi01>javac -d bin src/DemoLooping2.java
+//E:\Dropbox\kuliah\smester4\Pemrograman 1\sesi01>java -cp bin Latihan01.sesi1.bin.DemoLooping2
+//E:\Dropbox\kuliah\smester4\Pemrograman 1\sesi01>git add "src/DemoLooping2.java"
+//E:\Dropbox\kuliah\smester4\Pemrograman 1\sesi01>git commit -m "commit01"
+//E:\Dropbox\kuliah\smester4\Pemrograman 1\sesi01>git push origin1 master
+
 package Latihan01.sesi1.bin;
 import java.util.Random;
 
